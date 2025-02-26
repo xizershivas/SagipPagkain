8ffdad8a56415e71881de0006d3809c6eeb432a2
torph3uz <torph3uz@gmail.com>
2025-02-27 00:53:55 +0800

    Fixed sagiplogo.png directory and footer

--- src/web/global/footer.php
+++ src/web/global/footer.php
@@ -21,13 +21,6 @@
 
 <div class="container copyright text-center mt-4">
   <p>© <span>Copyright</span> <strong class="sitename">Sagip pagkain</strong> <span>All Rights Reserved</span></p>
-  <div class="credits">
-    <!-- All the links in the footer should remain intact. -->
-    <!-- You can delete the links only if you've purchased the pro version. -->
-    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-    <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-  </div>
 </div>
 
 </footer>
