8866f667d41dc369173806a2a1debf539c73a243
torph3uz <torph3uz@gmail.com>
2025-05-27 23:54:18 +0800

    JS fixes for inventory management expiration date

--- app/js/inventoryManagement.js
+++ app/js/inventoryManagement.js
@@ -2,7 +2,7 @@ const filterBy = document.querySelector('#filterBy');
 const searchItem = document.querySelector("#searchItem");
 const filterOptions = document.querySelector('#filterOptions');
 const tableBody = document.querySelector('#tableBody');
-
+ 
 // Show inventory table data
 function setTableData({ inventoryData }) {
     tableBody.innerHTML = '';
@@ -10,7 +10,7 @@ function setTableData({ inventoryData }) {
         const tr = document.createElement('tr');
         const td = document.createElement('td');
         td.textContent = "No records found";
-        td.setAttribute('colspan', 6);
+        td.setAttribute('colspan', 7);
         td.style.textAlign = 'center';
         tr.append(td);
         tableBody.append(tr);
@@ -18,40 +18,43 @@ function setTableData({ inventoryData }) {
     else {
         inventoryData.forEach((d, index) => {
             index++;
-
+ 
             const tr = document.createElement('tr');
             const tdRowNo = document.createElement('td');
             const tdItem = document.createElement('td');
             const tdQuantity = document.createElement('td');
             const tdUnit = document.createElement('td');
             const tdCategory = document.createElement('td');
+            const tdExpiDate = document.createElement('td');
             const tdFoodBank = document.createElement('td');
-
+ 
             tdRowNo.textContent = index;
             tdItem.textContent = d.strItem;
             tdQuantity.textContent = d.intQuantity;
             tdUnit.textContent = d.strUnit;
             tdCategory.textContent = d.strCategory;
+            tdExpiDate.textContent = d.dtmExpirationDate;
             tdFoodBank.textContent = d.strFoodBank;
-
+ 
             tr.append(tdRowNo);
             tr.append(tdItem);
             tr.append(tdQuantity);
             tr.append(tdUnit);
             tr.append(tdCategory);
+            tr.append(tdExpiDate);
             tr.append(tdFoodBank);
-
+ 
             tableBody.append(tr);
         });
     }
 }
-
+ 
 function inputSearch(e) {
     let search = e ? e.currentTarget.value : "";
     let filter = filterBy.value;
-
+ 
     const xmlhttp = new XMLHttpRequest();
-
+ 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
             const response = JSON.parse(this.responseText);
@@ -59,18 +62,18 @@ function inputSearch(e) {
                 setDataListOptions(response.data, filter);
                 setTableData(response.data);
             } else {
-
+ 
             }
         }
     };
-
+ 
     xmlhttp.open('GET', `../../../app/controllers/inventoryManagement.php?filter=${filter}&search=${search}`, true);
     xmlhttp.send(); 
 }
-
+ 
 function setDataListOptions({ dataListOptions }, filter) {
     filterOptions.innerHTML = '';
-
+ 
     dataListOptions.forEach(r => {
         const option = document.createElement('option');
         switch(filter) {
@@ -82,13 +85,12 @@ function setDataListOptions({ dataListOptions }, filter) {
         filterOptions.append(option);
     });
 }
-
+ 
 function filterOption(e) {
     searchItem.value = '';
     let filter = e ? e.currentTarget.value : "strCategory";
-    
     const xmlhttp = new XMLHttpRequest();
-
+ 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
             const response = JSON.parse(this.responseText);
@@ -96,15 +98,15 @@ function filterOption(e) {
                 setDataListOptions(response.data, filter);
                 setTableData(response.data);
             } else {
-
+ 
             }
         }
     };
-
+ 
     xmlhttp.open('GET', `../../../app/controllers/inventoryManagement.php?filter=${filter}`, true);
     xmlhttp.send();
 }
-
+ 
 filterBy.addEventListener('change', filterOption);
 searchItem.addEventListener('input', inputSearch);
 searchItem.addEventListener('keydown', (e) => {
@@ -112,7 +114,7 @@ searchItem.addEventListener('keydown', (e) => {
         e.preventDefault();
     }
 });
-
+ 
 window.onload = function() {
     filterOption(); // triggered on page load to populate datalist
 }
